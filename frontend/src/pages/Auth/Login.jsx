d323cd20e7b4cc478e71465ce73b9ea05ebc81bd
sahiba0915 <sahibakumari0915@outlook.com>
2025-05-05 19:20:05 +0530

    fix: path

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import AuthLayout from '../../components/Layouts/AuthLayout'
+import AuthLayout from "../../components/layouts/AuthLayout"
 import { Link, useNavigate } from 'react-router-dom';
 import Input from '../../components/Inputs/Input';
 import { validateEmail } from '../../utils/helper';

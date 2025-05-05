dc49dffa34824883edb1f1afb75860062f875225
sahiba0915 <sahibakumari0915@outlook.com>
2025-05-05 17:55:00 +0530

    fix: styling

--- frontend/src/components/layouts/SideMenu.jsx
+++ frontend/src/components/layouts/SideMenu.jsx
@@ -47,7 +47,7 @@ const SideMenu = ({activeMenu}) => {
       {SIDE_MENU_DATA.map((item,index) => (
         <button
         key={`menu_${index}`}
-        className={`w-full flex items-center gap-4 text-[15px] ${activeMenu == item.label ? "text-white bg-primary": "" } py-3 px-6 rounded-lg mb-3`}
+        className={`w-full flex items-center gap-4 text-[15px] ${activeMenu == item.label ? "text-white bg-primary": "" } py-3 px-6 rounded-lg mb-3  cursor-pointer`}
         onClick={() => handleClick(item.path)}>
             <item.icon className='text-xl'/>
             {item.label}

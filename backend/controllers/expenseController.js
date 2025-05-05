0fbe0d262506e24459ad8ce82d0b1a66c29b446f
sahiba0915 <sahibakumari0915@outlook.com>
2025-05-05 17:51:40 +0530

    fix: expense and income controller fix

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -67,7 +67,11 @@ exports.downloadExpenseExcel = async(req, res) => {
         const wb = xlsx.utils.book_new();
         const ws = xlsx.utils.json_to_sheet(data);
         xlsx.utils.book_append_sheet(wb,ws, "Expense");
-        xlsx.writeFile(wb, 'expense_details.xlsx');
+        const buffer = xlsx.write(wb, { type: "buffer", bookType: "xlsx" });
+
+        res.setHeader("Content-Disposition", "attachment; filename=expense_details.xlsx");
+        res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        res.send(buffer);
     }catch(error){
         res.status(500).json({message: "Server Error"});
     }
